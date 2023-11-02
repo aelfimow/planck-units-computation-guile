cc25c5c8ed6cfc41afade9cad99fb54240934f27
Alexander Elfimow <email-not-set@unknown.de>
2023-11-02 19:35:11 +0100

    Planck-time: added script

---
+++ Planck-time/Planck-time.scm
@@ -0,0 +1,12 @@
+#! /usr/bin/guile -s
+!#
+
+(define pi 3.1415926535897932384626433832795)
+(define h 6.62607015E-34)
+(define h_reduced (/ h (* 2.0 pi)))
+(define G 6.67430E-11)
+(define c 299792458.0)
+(define Planck-time (sqrt (/ (* h_reduced G) (expt c 5.0))))
+(define result-str (string-append "Planck time " (number->string Planck-time)))
+(display result-str)
+(newline)

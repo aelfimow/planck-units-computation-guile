b41961acc4840e049d3cac7473826419789e1174
Alexander Elfimow <email-not-set@unknown.de>
2023-11-02 19:29:03 +0100

    Planck-mass: added script

---
+++ Planck-mass/Planck-mass.scm
@@ -0,0 +1,12 @@
+#! /usr/bin/guile -s
+!#
+
+(define pi 3.1415926535897932384626433832795)
+(define h 6.62607015E-34)
+(define h_reduced (/ h (* 2.0 pi)))
+(define G 6.67430E-11)
+(define c 299792458.0)
+(define mp (sqrt (/ (* h_reduced c) G)))
+(define result-str (string-append "Planck mass " (number->string mp)))
+(display result-str)
+(newline)

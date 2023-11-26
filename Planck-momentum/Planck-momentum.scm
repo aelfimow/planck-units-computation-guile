2af04e650ee4b3e2fb00a1a9196555d314182cab
Alexander Elfimow <email-not-set@unknown.de>
2023-11-26 19:03:54 +0100

    Planck-momentum: added script

---
+++ Planck-momentum/Planck-momentum.scm
@@ -0,0 +1,12 @@
+#! /usr/bin/guile -s
+!#
+
+(define pi 3.1415926535897932384626433832795)
+(define h 6.62607015E-34)
+(define h_reduced (/ h (* 2.0 pi)))
+(define G 6.67430E-11)
+(define c 299792458.0)
+(define Planck-momentum (sqrt (/ (* h_reduced (expt c 3.0)) G)))
+(define result-str (string-append "Planck momentum " (number->string Planck-momentum)))
+(display result-str)
+(newline)
